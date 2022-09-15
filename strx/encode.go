95f9efdbf77991ff9b0202389495ce75c8c10fe4
fzdwx <likelovec@gmail.com>
2022-09-15 10:10:43 +0800

    feat

---
+++ strx/encode.go
@@ -0,0 +1,26 @@
+package strx
+
+import (
+	"net/url"
+	"strings"
+)
+
+// URLEncode encode url string.
+func URLEncode(s string) string {
+	if pos := strings.IndexRune(s, '?'); pos > -1 { // escape query data
+		return s[0:pos+1] + url.QueryEscape(s[pos+1:])
+	}
+	return s
+}
+
+// URLDecode decode url string.
+func URLDecode(s string) string {
+	if pos := strings.IndexRune(s, '?'); pos > -1 { // un-escape query data
+		qy, err := url.QueryUnescape(s[pos+1:])
+		if err == nil {
+			return s[0:pos+1] + qy
+		}
+	}
+
+	return s
+}
